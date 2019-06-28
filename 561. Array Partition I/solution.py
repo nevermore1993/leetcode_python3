06c826bde16f657082205ef80f0aec5ac2d6e6f6
szz <34294200+nevermore1993@users.noreply.github.com>
2019-06-28 14:57:06 +0800

    Create solution.py

---
+++ 561. Array Partition I/solution.py
@@ -0,0 +1,9 @@
+// 要使所有的对中的小的元素的和最大，那么我们要尽量让小的元素大。最大的元素不可能是小的，而第二大的元素和最大的元素配对，那么他就是小的。
+// 同理，第四大的元素和第三大的元素配对，第四大的就是小的。以此类推。先排序，再将所有奇数位置的数相加。
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 0
+        for i in range(0,len(nums),2):
+            res += nums[i]
+        return res
