d94087947de53925b244bba846978ed7aa146f15
szz <34294200+nevermore1993@users.noreply.github.com>
2019-11-01 20:31:07 +0800

    Update solution.py

--- 347. Top K Frequent Elements/solution.py
+++ 347. Top K Frequent Elements/solution.py
@@ -12,6 +12,7 @@ class Solution:
         
 // 先统计每个数字出现的次数，再放k个数字进入heapq中，在加入新的元素到heapq中时，会自动排序。如果新加入的元素比heapq中最小的要大
 // 那么就替换最小的，如果比最小的还小，就跳过。这样不用排序，我们就得到了前k个最大的。
+// 这里的h就是小顶堆，heapq.heappush(h, d[i])就是将元素d[i]添加到h中，heappop(h)就是弹出堆顶元素。
 from collections import Counter
 import heapq
 
